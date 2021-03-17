9a07e5149e2b29d3d52a3a93b84e4c7df6a1185e
ZmbOrk <aleksei05030@gmail.com>
2021-03-17 15:39:01 +1000

    Add comment on code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        boolean ticket = true;
-        int bonus_miles = ticket ? 20 : 0;
-        int price_ticket = 12000;
-        int free_miles = 12000 / bonus_miles;
 
-        System.out.println("Your bonus miles " + free_miles);
+        boolean ticket = true; // Если билет куплен = тру
+        int bonus_miles = ticket ? 20 : 0; // Бонусные мили насчитываются, если билет куплен
+        int price_ticket = 12000; // цена билета
+        int free_miles = 12000 / bonus_miles; // формула расчета для получения бонусных милей
+
+        System.out.println("Your bonus miles " + free_miles); //вывод результата
 
 
     }
